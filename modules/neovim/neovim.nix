36c15e671d04602594826149e8300bf783f1f456
mclrc <moritzamando@proton.me>
2025-07-07 00:34:23 +0200

    fix neovim

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [ neovim ];
 
-  xdg.configFile."neovim" = {
+  xdg.configFile."nvim" = {
     source = ./neovim-config;
     recursive = true;         # Ensure all files in the folder are copied
   };
