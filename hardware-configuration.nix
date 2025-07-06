8feedbb5a583b77d8df33d9f6e186079041b06e6
mclrc <moritzamando@proton.me>
2025-07-06 23:29:33 +0200

    remove deprecated option

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -40,7 +40,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
+  hardware.graphics.enable = true;
 }
